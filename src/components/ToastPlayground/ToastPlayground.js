462d33510d3cf69a6f58a49fea7998e7c83ac17c
João Víctor de Araujo Lima <joaovictor.ne@gmail.com>
2025-11-24 13:14:42 -0300

    Toast: Add toasts on ToastPlayground and updated component

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -9,9 +10,16 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [toasts, setToasts] = React.useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
+    setToasts([
+      ...toasts,
+      { id: crypto.randomUUID(), message, variant },
+    ]);
+    setMessage("");
+    setVariant(VARIANT_OPTIONS[0]);
   }
 
   return (
@@ -20,6 +28,18 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {toasts.map((toast) => (
+        <Toast
+          key={toast.id}
+          variant={toast.variant}
+          toastId={toast.id}
+          toastList={toasts}
+          setToastList={setToasts}
+        >
+          {toast.message}
+        </Toast>
+      ))}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -36,6 +56,7 @@ function ToastPlayground() {
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               className={styles.messageInput}
+              required
             />
           </div>
         </div>
